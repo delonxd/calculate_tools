3872d7dd8abc797d878d9c726bc65e6b7be0030f
李继隆 <delong1q2w3e@163.com>
2020-11-23 10:17:08 +0800

    添加了新内容

--- test.py
+++ test.py
@@ -267,243 +267,243 @@ class EdgeWire(Edge):
         pass
 
 
-class Port:
-    def __init__(self, edge, flag: bool):
-        self.edge = edge
-        self.start_flag = flag
-
-    @property
-    def node(self):
-        if self.start_flag:
-            return self.edge.start
-        else:
-            return self.edge.end
-
-    def link_node(self, other):
-        self.node.link_node(other)
-
-
-class Module:
-    def __init__(self, parent, name_base):
-        self.parent = parent
-        self.name_base = name_base
-        self.name = ''
-
-        self.ports = list()
-        self.modules = list()
-        self.edges = list()
-        # self.edges_all = list()
-
-    def get_edges(self):
-        edges = self.edges.copy()
-        for ele in self.modules:
-            edges.extend(ele.get_edges())
-        return edges
-
-    def get_nodes(self):
-        nodes = set()
-        # for edge in self.edges:
-        #     nodes.add(edge.start)
-        #     nodes.add(edge.end)
-        # for module in self.modules:
-        #     nodes.update(module.get_nodes())
-        for edge in self.get_edges():
-            nodes.add(edge.start)
-            nodes.add(edge.end)
-        return nodes
-
-    def print_edges(self):
-        name_list = list()
-        for edge in self.get_edges():
-            name_list.append(edge.name)
-        name_list.sort()
-        print(name_list)
-
-    def print_nodes(self):
-        name_list = list()
-        for node in self.get_nodes():
-            name_list.append(node.name)
-            print(node.name)
-            node.print_edges()
-
-    def config_port(self, *ports):
-        for port in ports:
-            if isinstance(port, Port):
-                self.ports.append(port)
-            else:
-                raise KeyboardInterrupt("类型异常：需要Port类型")
-
-    # def config_port(self, *nodes):
-    #     for node in nodes:
-    #         if isinstance(node, Node):
-    #             self.ports.append(node)
-    #         else:
-    #             raise KeyboardInterrupt("类型异常：需要Node类型")
+# class Port:
+#     def __init__(self, edge, flag: bool):
+#         self.edge = edge
+#         self.start_flag = flag
+#
+#     @property
+#     def node(self):
+#         if self.start_flag:
+#             return self.edge.start
+#         else:
+#             return self.edge.end
+#
+#     def link_node(self, other):
+#         self.node.link_node(other)
 
 
-    def add_element(self, *element):
-        for ele in element:
-            if isinstance(ele, Edge):
-                self.edges.append(ele)
-                ele.parent = self
-            elif isinstance(ele, Module):
-                self.modules.append(ele)
-                ele.parent = self
-            else:
-                raise KeyboardInterrupt("类型异常：需要Edge或Module类型")
-
-    def config_edge_para(self, freq):
-        pass
-
-    def config_parameter(self, freq):
-        for md in self.modules:
-            md.config_parameter(freq)
-        self.config_edge_para(freq)
-
-    def create_circuit(self):
-        pass
-
-    def create_port(self):
-        pass
-
-    def config_name(self):
-        if self.parent:
-            self.name = self.parent.name + '_' + self.name_base
-        else:
-            self.name = self.name_base
-        for edge in self.edges:
-            edge.config_name()
-        for module in self.modules:
-            module.config_name()
-
-
-class ModuleImpedance(Module):
-    def __init__(self, parent, name_base, z):
-        super().__init__(parent, name_base)
-        self.z = z
-        self.r1 = EdgeResistance(self, '阻抗', 0)
-
-        self.add_element(self.r1)
-
-        self.create_circuit()
-        self.create_port()
-
-    def create_circuit(self):
-        pass
-
-    def create_port(self):
-        self.config_port(Port(self.r1, True), Port(self.r1, False))
-
-    def config_edge_para(self, freq):
-        self.r1.config_parameter(self.z[freq])
-
-
-class Cable(Module):
-    def __init__(self, parent, name_base, length, cab_r, cab_l, cab_c):
-        super().__init__(parent, name_base)
-        self.R = cab_r
-        self.L = cab_l
-        self.C = cab_c
-        self.length = length
-
-        self.r1 = EdgeResistance(self, 'R1', 0)
-        self.r2 = EdgeResistance(self, 'R2', 0)
-        self.rp1 = EdgeResistance(self, 'Rp1', 0)
-        self.rp2 = EdgeResistance(self, 'Rp2', 0)
-        self.add_element(self.r1, self.r2, self.rp1, self.rp2)
-
-        self.create_circuit()
-        self.create_port()
-
-    def create_circuit(self):
-        self.r1.start.link_node(self.rp1.start)
-        self.r2.start.link_node(self.rp1.end)
-        self.r1.end.link_node(self.rp2.start)
-        self.r2.end.link_node(self.rp2.end)
-
-    def create_port(self):
-        self.config_port(Port(self.r1, True), Port(self.r2, True),
-                         Port(self.r1, False), Port(self.r2, False))
-
-    def config_edge_para(self, freq):
-        length = float(self.length)
-        w = 2 * np.pi * freq
-        z0 = float(self.R) + 1j * w * float(self.L)
-        y0 = 10e-10 + 1j * w * float(self.C)
-        # y0 = 1j * w * float(self.C)
-        zc = np.sqrt(z0 / y0)
-        gama = np.sqrt(z0 * y0)
-        zii = zc * np.sinh(gama * length)
-        yii = (np.cosh(gama * length) - 1) / zc / np.sinh(gama * length)
-
-        self.r1.config_parameter(zii/2)
-        self.r2.config_parameter(zii/2)
-        self.rp1.config_parameter(1/yii)
-        self.rp2.config_parameter(1/yii)
-
-
-class Transformer(Module):
-    def __init__(self, parent, name_base, n):
-        super().__init__(parent, name_base)
-        self.n = n
-        self.w1 = EdgeWinding(self, '1原边')
-        self.w2 = EdgeWinding(self, '2副边')
-        self.add_element(self.w1, self.w2)
-
-        self.create_circuit()
-        self.create_port()
-
-    def create_circuit(self):
-        pass
-
-    def create_port(self):
-        self.config_port(Port(self.w1, True), Port(self.w1, False),
-                         Port(self.w2, True), Port(self.w2, False))
-
-    def config_edge_para(self, freq):
-        self.w1.config_parameter(self.w2, self.n)
-        self.w2.config_parameter(self.w1, 1 / self.n)
-        # self.w1.source = True
-        # self.w2.source = False
-
-
-class TwoPortCircuitPi(Module):
-    def __init__(self, parent, name_base, z1, z2, z3):
-        super().__init__(parent, name_base)
-        self.r1 = ModuleImpedance(self, 'R1', z1)
-        self.r2 = ModuleImpedance(self, 'R2', z2)
-        self.r3 = ModuleImpedance(self, 'R3', z3)
-        self.add_element(self.r1, self.r2, self.r3)
-        self.create_circuit()
-        self.create_port()
-
-    def create_circuit(self):
-        self.r1.ports[0].link_node(self.r2.ports[0])
-        self.r2.ports[1].link_node(self.r3.ports[0])
-        self.r1.ports[1].link_node(self.r3.ports[1])
-
-    def create_port(self):
-        self.config_port(self.r1.ports[0], self.r2.ports[1],
-                         self.r3.ports[0], self.r3.ports[1])
-
-
-class TwoPortCircuitT(Module):
-    def __init__(self, parent, name_base, z1, z2, z3):
-        super().__init__(parent, name_base)
-        self.r1 = ModuleImpedance(self, 'R1', z1)
-        self.r2 = ModuleImpedance(self, 'R2', z2)
-        self.r3 = ModuleImpedance(self, 'R3', z3)
-        self.add_element(self.r1, self.r2, self.r3)
-        self.create_circuit()
-        self.create_port()
-
-    def create_circuit(self):
-        self.r1.ports[1].link_node(self.r3.ports[0])
-        self.r1.ports[1].link_node(self.r2.ports[0])
-
-    def create_port(self):
-        self.config_port(self.r1.ports[0], self.r2.ports[1],
-                         self.r3.ports[1], self.r2.ports[1])
+# class Module:
+#     def __init__(self, parent, name_base):
+#         self.parent = parent
+#         self.name_base = name_base
+#         self.name = ''
+#
+#         self.ports = list()
+#         self.modules = list()
+#         self.edges = list()
+#         # self.edges_all = list()
+#
+#     def get_edges(self):
+#         edges = self.edges.copy()
+#         for ele in self.modules:
+#             edges.extend(ele.get_edges())
+#         return edges
+#
+#     def get_nodes(self):
+#         nodes = set()
+#         # for edge in self.edges:
+#         #     nodes.add(edge.start)
+#         #     nodes.add(edge.end)
+#         # for module in self.modules:
+#         #     nodes.update(module.get_nodes())
+#         for edge in self.get_edges():
+#             nodes.add(edge.start)
+#             nodes.add(edge.end)
+#         return nodes
+#
+#     def print_edges(self):
+#         name_list = list()
+#         for edge in self.get_edges():
+#             name_list.append(edge.name)
+#         name_list.sort()
+#         print(name_list)
+#
+#     def print_nodes(self):
+#         name_list = list()
+#         for node in self.get_nodes():
+#             name_list.append(node.name)
+#             print(node.name)
+#             node.print_edges()
+#
+#     def config_port(self, *ports):
+#         for port in ports:
+#             if isinstance(port, Port):
+#                 self.ports.append(port)
+#             else:
+#                 raise KeyboardInterrupt("类型异常：需要Port类型")
+#
+#     # def config_port(self, *nodes):
+#     #     for node in nodes:
+#     #         if isinstance(node, Node):
+#     #             self.ports.append(node)
+#     #         else:
+#     #             raise KeyboardInterrupt("类型异常：需要Node类型")
+#
+#
+#     def add_element(self, *element):
+#         for ele in element:
+#             if isinstance(ele, Edge):
+#                 self.edges.append(ele)
+#                 ele.parent = self
+#             elif isinstance(ele, Module):
+#                 self.modules.append(ele)
+#                 ele.parent = self
+#             else:
+#                 raise KeyboardInterrupt("类型异常：需要Edge或Module类型")
+#
+#     def config_edge_para(self, freq):
+#         pass
+#
+#     def config_parameter(self, freq):
+#         for md in self.modules:
+#             md.config_parameter(freq)
+#         self.config_edge_para(freq)
+#
+#     def create_circuit(self):
+#         pass
+#
+#     def create_port(self):
+#         pass
+#
+#     def config_name(self):
+#         if self.parent:
+#             self.name = self.parent.name + '_' + self.name_base
+#         else:
+#             self.name = self.name_base
+#         for edge in self.edges:
+#             edge.config_name()
+#         for module in self.modules:
+#             module.config_name()
+#
+#
+# class ModuleImpedance(Module):
+#     def __init__(self, parent, name_base, z):
+#         super().__init__(parent, name_base)
+#         self.z = z
+#         self.r1 = EdgeResistance(self, '阻抗', 0)
+#
+#         self.add_element(self.r1)
+#
+#         self.create_circuit()
+#         self.create_port()
+#
+#     def create_circuit(self):
+#         pass
+#
+#     def create_port(self):
+#         self.config_port(Port(self.r1, True), Port(self.r1, False))
+#
+#     def config_edge_para(self, freq):
+#         self.r1.config_parameter(self.z[freq])
+#
+#
+# class Cable(Module):
+#     def __init__(self, parent, name_base, length, cab_r, cab_l, cab_c):
+#         super().__init__(parent, name_base)
+#         self.R = cab_r
+#         self.L = cab_l
+#         self.C = cab_c
+#         self.length = length
+#
+#         self.r1 = EdgeResistance(self, 'R1', 0)
+#         self.r2 = EdgeResistance(self, 'R2', 0)
+#         self.rp1 = EdgeResistance(self, 'Rp1', 0)
+#         self.rp2 = EdgeResistance(self, 'Rp2', 0)
+#         self.add_element(self.r1, self.r2, self.rp1, self.rp2)
+#
+#         self.create_circuit()
+#         self.create_port()
+#
+#     def create_circuit(self):
+#         self.r1.start.link_node(self.rp1.start)
+#         self.r2.start.link_node(self.rp1.end)
+#         self.r1.end.link_node(self.rp2.start)
+#         self.r2.end.link_node(self.rp2.end)
+#
+#     def create_port(self):
+#         self.config_port(Port(self.r1, True), Port(self.r2, True),
+#                          Port(self.r1, False), Port(self.r2, False))
+#
+#     def config_edge_para(self, freq):
+#         length = float(self.length)
+#         w = 2 * np.pi * freq
+#         z0 = float(self.R) + 1j * w * float(self.L)
+#         y0 = 10e-10 + 1j * w * float(self.C)
+#         # y0 = 1j * w * float(self.C)
+#         zc = np.sqrt(z0 / y0)
+#         gama = np.sqrt(z0 * y0)
+#         zii = zc * np.sinh(gama * length)
+#         yii = (np.cosh(gama * length) - 1) / zc / np.sinh(gama * length)
+#
+#         self.r1.config_parameter(zii/2)
+#         self.r2.config_parameter(zii/2)
+#         self.rp1.config_parameter(1/yii)
+#         self.rp2.config_parameter(1/yii)
+#
+#
+# class Transformer(Module):
+#     def __init__(self, parent, name_base, n):
+#         super().__init__(parent, name_base)
+#         self.n = n
+#         self.w1 = EdgeWinding(self, '1原边')
+#         self.w2 = EdgeWinding(self, '2副边')
+#         self.add_element(self.w1, self.w2)
+#
+#         self.create_circuit()
+#         self.create_port()
+#
+#     def create_circuit(self):
+#         pass
+#
+#     def create_port(self):
+#         self.config_port(Port(self.w1, True), Port(self.w1, False),
+#                          Port(self.w2, True), Port(self.w2, False))
+#
+#     def config_edge_para(self, freq):
+#         self.w1.config_parameter(self.w2, self.n)
+#         self.w2.config_parameter(self.w1, 1 / self.n)
+#         # self.w1.source = True
+#         # self.w2.source = False
+#
+#
+# class TwoPortCircuitPi(Module):
+#     def __init__(self, parent, name_base, z1, z2, z3):
+#         super().__init__(parent, name_base)
+#         self.r1 = ModuleImpedance(self, 'R1', z1)
+#         self.r2 = ModuleImpedance(self, 'R2', z2)
+#         self.r3 = ModuleImpedance(self, 'R3', z3)
+#         self.add_element(self.r1, self.r2, self.r3)
+#         self.create_circuit()
+#         self.create_port()
+#
+#     def create_circuit(self):
+#         self.r1.ports[0].link_node(self.r2.ports[0])
+#         self.r2.ports[1].link_node(self.r3.ports[0])
+#         self.r1.ports[1].link_node(self.r3.ports[1])
+#
+#     def create_port(self):
+#         self.config_port(self.r1.ports[0], self.r2.ports[1],
+#                          self.r3.ports[0], self.r3.ports[1])
+#
+#
+# class TwoPortCircuitT(Module):
+#     def __init__(self, parent, name_base, z1, z2, z3):
+#         super().__init__(parent, name_base)
+#         self.r1 = ModuleImpedance(self, 'R1', z1)
+#         self.r2 = ModuleImpedance(self, 'R2', z2)
+#         self.r3 = ModuleImpedance(self, 'R3', z3)
+#         self.add_element(self.r1, self.r2, self.r3)
+#         self.create_circuit()
+#         self.create_port()
+#
+#     def create_circuit(self):
+#         self.r1.ports[1].link_node(self.r3.ports[0])
+#         self.r1.ports[1].link_node(self.r2.ports[0])
+#
+#     def create_port(self):
+#         self.config_port(self.r1.ports[0], self.r2.ports[1],
+#                          self.r3.ports[1], self.r2.ports[1])
 
 
 class TcsrTransformer(Module):
